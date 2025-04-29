7e989309216214cdfed2fe70acae67b548b7945b
Nikhilsingh0408 <nikhilrewa6261@gmail.com>
2025-04-30 01:07:42 +0530

    Some Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { FiMenu, FiX, FiCopy, FiRefreshCw, FiCode, FiTerminal, FiPlay } from "re
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://real-time-code-editor-oe3x.onrender.com", {
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
